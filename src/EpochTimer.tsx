195bf8e9a4167e963fd3fc09a8c217d4d38df355
Linus Björklund <weedzcokie@gmail.com>
2023-08-24 18:11:06 +0200

    Add time string to epoch timer

--- src/EpochTimer.tsx
+++ src/EpochTimer.tsx
@@ -1,14 +1,19 @@
 import { createSignal, onCleanup } from "solid-js";
 
 export function EpochTimer() {
-    const [time, setTime] = createSignal(Date.now());
+    const [time, setTime] = createSignal(new Date());
 
-    let timer = window.setInterval(() => setTime(Date.now()), 1000);
-
+    let timer = window.setInterval(() => setTime(new Date()), 1000);
 
     onCleanup(() => window.clearInterval(timer));
 
     return (
-        <p>Current unix epoch time: <span class="font-mono" onMouseOver={() => clearInterval(timer)} onMouseLeave={() => timer = window.setInterval(() => setTime(Date.now()), 1000)}>{Math.floor(time() / 1000)}</span></p>
+        <div
+            onMouseOver={() => clearInterval(timer)}
+            onMouseLeave={() => timer = window.setInterval(() => setTime(new Date()), 1000)}
+        >
+            <p>Current unix epoch time: <span class="font-mono">{Math.floor(time().getTime() / 1000)}</span></p>
+            <p>{time().toISOString()}</p>
+        </div>
     );
 }
