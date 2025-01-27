5c611be25d21ea5c993e3872bcb642219da9891a
Linus Björklund <weedzcokie@gmail.com>
2025-01-27 22:57:57 +0100

    fix: Update timer code
    Might be better (?)

--- src/EpochTimer.tsx
+++ src/EpochTimer.tsx
@@ -1,19 +1,30 @@
 import { createSignal, onCleanup } from "solid-js";
 
+
 export function EpochTimer() {
-    const [time, setTime] = createSignal(new Date());
+  const [time, setTime] = createSignal(new Date());
 
-    let timer = window.setInterval(() => setTime(new Date()), 1000);
+  let timer: number;
+  setTimer();
 
-    onCleanup(() => window.clearInterval(timer));
+  // TODO: Figure out a better timer or something..
+  function setTimer() {
+    setTime(new Date());
+    const now = Date.now();
+    const nextSec = Math.round((now + 1000) / 1000) * 1000;
+    const diff = nextSec - now;
+    timer = window.setTimeout(setTimer, diff + 10);
+  }
 
-    return (
-        <div
-            onMouseOver={() => clearInterval(timer)}
-            onMouseLeave={() => timer = window.setInterval(() => setTime(new Date()), 1000)}
-        >
-            <p>Current unix epoch time: <span class="font-mono">{Math.floor(time().getTime() / 1000)}</span></p>
-            <p>{time().toISOString()}</p>
-        </div>
-    );
+  onCleanup(() => window.clearTimeout(timer));
+
+  return (
+    <div
+      onMouseOver={() => clearTimeout(timer)}
+      onMouseLeave={setTimer}
+    >
+      <p>Current unix epoch time: <span class="font-mono">{Math.floor(time().getTime() / 1000)}</span></p>
+      <p>{time().toISOString()}</p>
+    </div >
+  );
 }
